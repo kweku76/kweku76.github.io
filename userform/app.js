c07c9b8dfc8efdcfd9eea352c873a9132a30a6aa
kweku76 <daffran@gmail.com>
2020-05-01 21:19:51 +1000

    Update app.js

--- userform/app.js
+++ userform/app.js
@@ -4,42 +4,42 @@ document.getElementById('zip').addEventListener('blur', validateZip);
 document.getElementById('email').addEventListener('blur', validateEmail);
 document.getElementById('phone').addEventListener('blur', validatePhone);
 
-function validateName() {
+function validateName() { //on blur event get value of name
   const name = document.getElementById('name');
-  const re = /^[a-zA-Z]{2,10}$/;
+  const re = /^[a-zA-Z]{2,10}$/; //variable for regular expression - name should be letters
 
-  if (!re.test(name.value)) {
-    name.classList.add('is-invalid');
+  if (!re.test(name.value)) { //to evaluate above
+    name.classList.add('is-invalid'); //add the is-invalid class to show error
   } else {
-    name.classList.remove('is-invalid');
+    name.classList.remove('is-invalid'); //or remove error if input is valid
   }
 }
 
-function validateZip() {
+function validateZip() { // US zip code format
   const zip = document.getElementById('zip');
-  const re = /^[0-9]{5}(-[0-9]{4})?$/;
+  const re = /^[0-9]{5}(-[0-9]{4})?$/; // 66666-2222 zip format
 
-  if (!re.test(zip.value)) {
+  if (!re.test(zip.value)) { //same process as validateName
     zip.classList.add('is-invalid');
   } else {
     zip.classList.remove('is-invalid');
   }
 }
 
-function validateEmail() {
+function validateEmail() { //email address
   const email = document.getElementById('email');
-  const re = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/;
+  const re = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/; // brad@gmail.com 
 
-  if (!re.test(email.value)) {
+  if (!re.test(email.value)) { //same validation process
     email.classList.add('is-invalid');
   } else {
     email.classList.remove('is-invalid');
   }
 }
 
-function validatePhone() {
+function validatePhone() { //US phone number
   const phone = document.getElementById('phone');
-  const re = /^\(?\d{3}\)?[-. ]?\d{3}[-. ]?\d{4}$/;
+  const re = /^\(?\d{3}\)?[-. ]?\d{3}[-. ]?\d{4}$/; // regular expression form multiple formats - 555-555-5555 
 
   if (!re.test(phone.value)) {
     phone.classList.add('is-invalid');
