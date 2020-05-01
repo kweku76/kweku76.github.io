847e4ec01ddc9c1950aaa2440ec2226329c10e38
kweku76 <daffran@gmail.com>
2020-05-01 20:57:21 +1000

    Update app.js

--- userform/app.js
+++ userform/app.js
@@ -1,4 +1,4 @@
-// Form Blur Event Listeners
+// Form Blur Event Listeners - listening for blur event to validate input
 document.getElementById('name').addEventListener('blur', validateName);
 document.getElementById('zip').addEventListener('blur', validateZip);
 document.getElementById('email').addEventListener('blur', validateEmail);
@@ -8,7 +8,7 @@ function validateName() {
   const name = document.getElementById('name');
   const re = /^[a-zA-Z]{2,10}$/;
 
-  if(!re.test(name.value)){
+  if (!re.test(name.value)) {
     name.classList.add('is-invalid');
   } else {
     name.classList.remove('is-invalid');
@@ -19,7 +19,7 @@ function validateZip() {
   const zip = document.getElementById('zip');
   const re = /^[0-9]{5}(-[0-9]{4})?$/;
 
-  if(!re.test(zip.value)){
+  if (!re.test(zip.value)) {
     zip.classList.add('is-invalid');
   } else {
     zip.classList.remove('is-invalid');
@@ -30,7 +30,7 @@ function validateEmail() {
   const email = document.getElementById('email');
   const re = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/;
 
-  if(!re.test(email.value)){
+  if (!re.test(email.value)) {
     email.classList.add('is-invalid');
   } else {
     email.classList.remove('is-invalid');
@@ -41,7 +41,7 @@ function validatePhone() {
   const phone = document.getElementById('phone');
   const re = /^\(?\d{3}\)?[-. ]?\d{3}[-. ]?\d{4}$/;
 
-  if(!re.test(phone.value)){
+  if (!re.test(phone.value)) {
     phone.classList.add('is-invalid');
   } else {
     phone.classList.remove('is-invalid');
